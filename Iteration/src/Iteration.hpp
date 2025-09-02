99e95e414641684e4d6c9cfed720e247eb8076bb
Hannes Krüger <kruegerh@kd-elektroniksysteme.de>
2025-09-02 15:05:05 +0200

    Finish Iteration

--- Iteration/src/Iteration.hpp
+++ Iteration/src/Iteration.hpp
@@ -1,31 +1,85 @@
-#include <iterator>
+//  Iteration:
+//  Required functions:
+//  it<T> it::operator++, T it::operator*, it::operator!=(const it&)
 
+#pragma once
+
+#include <type_traits>
+#include <utility>
+
+// Common alias for the result of ++t
+template <typename T> using preinc_result_t = decltype(++std::declval<T&>());
+
+// has_valid_preincrement: ++t is valid and returns T&
+template <typename, typename = void> struct has_valid_preincrement : std::false_type
+{
+};
+
+template <typename T>
+struct has_valid_preincrement<T, std::void_t<preinc_result_t<T>>>
+    : std::bool_constant<std::is_same_v<preinc_result_t<T>, T&>>
+{
+};
+
+template <typename T> inline constexpr bool has_valid_preincrement_v = has_valid_preincrement<T>::value;
+
+// has_valid_pointer_preincrement: T is a pointer AND ++t is valid and returns T&
+template <typename, typename = void> struct has_valid_pointer_preincrement : std::false_type
+{
+};
+
+template <typename T>
+struct has_valid_pointer_preincrement<T, std::void_t<preinc_result_t<T>>>
+    : std::bool_constant<std::is_pointer_v<T> && std::is_same_v<preinc_result_t<T>, T&>>
+{
+};
+
+template <typename T> inline constexpr bool has_valid_pointer_preincrement_v = has_valid_pointer_preincrement<T>::value;
 template <typename T> class Iterator
 {
-  T* ptr;
+private:
+  T val;
 
 public:
-  using iterator_category = std::forward_iterator_tag;
-  using value_type        = T;
-  using difference_type   = std::ptrdiff_t;
-  using pointer           = T*;
-  using reference         = T&;
+  Iterator(T obj) : val(obj) {}
 
-  Iterator(T* p) : ptr(p) {}
+  T operator*() const
+  {
+    return this->val;
+  }
 
-  T& operator*() const { return *ptr; }
-  T* operator->() const { return ptr; }
+  Iterator& operator++()
+  {
+    if constexpr (std::is_pointer_v<T>)
+    {
+      static_assert(has_valid_pointer_preincrement_v<decltype(this->val)>,
+                    "Type T must support operator++ and must result in T*!");
+      this->val = ++(*this->val);
+    }
+    else
+    {
+      static_assert(has_valid_preincrement_v<decltype(this->val)>,
+                    "Type T must support operator++ and must result in T!");
+      ++this->val;
+    }
+    return *this;
+  }
 
-  Iterator& operator++() { ++ptr; return *this; }
-  Iterator operator++(int) { MyIterator tmp = *this; ++(*this); return tmp; }
-
-  friend bool operator==(const Iterator& other) const { return ptr == other.ptr; }
-  friend bool operator!=(const Iterator& other) const { return ptr != other.ptr; }
+  bool operator!=(const Iterator& other) const
+  {
+    return this->val != other.val;
+  }
 };
 
 template <typename T> class Iterable
 {
 public:
-  virtual Iterator<T> begin() = 0;
-  virtual Iterator<T> end  () = 0;
+  virtual Iterator<T> begin() const = 0;
+  virtual Iterator<T> end() const = 0;
+};
+
+template <typename T> class IterationElement
+{
+public:
+  virtual T operator++() = 0;
 };
